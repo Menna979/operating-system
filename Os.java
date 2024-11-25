e81573317fde3e79b679e78eb1e38aa38d83bdc5
MennaOsama <mo04999672@gmail.com>
2024-11-25 16:29:27 +0200

    added

---
+++ Os.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.os;
+
+/**
+ *
+ * @author mena
+ */
+public class Os {
+
+    public static void main(String[] args) {
+        System.out.println("Menna Osama");
+    }
+}
